99929efdd734b5628ada86e0005302dd2af5c098
laflosh <l_florent_l@hotmail.fr>
2024-10-20 16:27:42 +0200

    Add service class

--- src/main/java/com/openclassrooms/dataLayer/repository/CommentRepository.java
+++ src/main/java/com/openclassrooms/dataLayer/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package com.openclassrooms.dataLayer.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.dataLayer.model.Comment;
 
+@Repository
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
 
 }
---
+++ src/main/java/com/openclassrooms/dataLayer/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.openclassrooms.dataLayer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+}
